108901486a29f6ac3d8d3b0dc84f1a5596f9270f
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2022-02-19 23:13:56 +0530

    Dynamic Dashborad completed v1

--- src/main/webapp/app/course/course.tsx
+++ src/main/webapp/app/course/course.tsx
@@ -1,49 +1,113 @@
 import './course.scss';
 
-import React, { useEffect } from 'react';
+import React, { Component, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { Card, Button } from 'react-bootstrap';
 
 import { REDIRECT_URL } from 'app/shared/util/url-utils';
 import { useAppSelector } from 'app/config/store';
+import { Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import CourseCard from './courseCard';
+// export const Course = () => {
+//   const history = useHistory();
+//   const account = useAppSelector(state => state.authentication.account);
+//   useEffect(() => {
+//     const redirectURL = localStorage.getItem(REDIRECT_URL);
+//     if (redirectURL) {
+//       localStorage.removeItem(REDIRECT_URL);
+//       location.href = `${location.origin}${redirectURL}`;
+//     }
+//   });
+//   //
+//   // fetch("http://localhost:8080/showAllCourses", {
+//   //   method: "GET"
+//   // }).then(response => response.json()).then(postMessage => {
+//   //   console.log("Hello Ayush");
+//   // })
+//
+//   return (
+//     <Card className="coursedetail" style={{ width: '18rem' }}>
+//       <Card.Img variant="top" width="276px" height="180px" src="content/images/img.png" />
+//       <Card.Body>
+//         <Card.Title>
+//           <h5>IT348-CRNS</h5>
+//         </Card.Title>
+//         <p>
+//           <i className="fas fa-calendar-day"></i>&nbsp;&nbsp;<span>10/01/2022</span>
+//         </p>
+//         <p>
+//           <i className="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;<span>Parth Shah</span>
+//         </p>
+//         <Button onClick={() => history.push('/assignments')} variant="primary">
+//           <h6>View Assignments</h6>
+//         </Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// };
 
-export const Course = () => {
-  const history = useHistory();
-  const account = useAppSelector(state => state.authentication.account);
-  useEffect(() => {
-    const redirectURL = localStorage.getItem(REDIRECT_URL);
-    if (redirectURL) {
-      localStorage.removeItem(REDIRECT_URL);
-      location.href = `${location.origin}${redirectURL}`;
-    }
-  });
-  //
-  // fetch("http://localhost:8080/showAllCourses", {
-  //   method: "GET"
-  // }).then(response => response.json()).then(postMessage => {
-  //   console.log("Hello Ayush");
-  // })
+class Course extends Component {
+  state = {
+    course: [],
+  };
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <Card className="coursedetail" style={{ width: '18rem' }}>
-      <Card.Img variant="top" width="276px" height="180px" src="content/images/img.png" />
-      <Card.Body>
-        <Card.Title>
-          <h5>IT348-CRNS</h5>
-        </Card.Title>
-        <p>
-          <i className="fas fa-calendar-day"></i>&nbsp;&nbsp;<span>10/01/2022</span>
-        </p>
-        <p>
-          <i className="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;<span>Parth Shah</span>
-        </p>
-        <Button onClick={() => history.push('/assignments')} variant="primary">
-          <h6>View Assignments</h6>
-        </Button>
-      </Card.Body>
-    </Card>
-  );
-};
+  componentDidMount() {
+    let bearer = 'Bearer ';
+    let token = sessionStorage.getItem('jhi-authenticationToken');
+
+    token = token.slice(1, -1);
+
+    bearer = bearer + token;
+
+    fetch('/api/courses', {
+      method: 'GET',
+      headers: {
+        accept: '*/*',
+        Authorization: bearer,
+      },
+    })
+      .then(response => response.json())
+      .then(result => {
+        this.setState({ course: result });
+        console.log(this.state);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <Row>
+        {this.state.course.map(course => (
+          <Card key={course.id} className="coursedetail" style={{ width: '18rem' }}>
+            <Card.Img variant="top" width="276px" height="180px" src="content/images/img.png" />
+            <Card.Body>
+              <Card.Title>
+                <h5>{course.courseTitle}</h5>
+              </Card.Title>
+              <p>
+                <i className="fas fa-calendar-day"></i>&nbsp;&nbsp;<span>{course.courseCreatedOn}</span>
+              </p>
+              <p>
+                <i className="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;<span>{course.courseDescription}</span>
+              </p>
+              <Link to="/assignments" className="btn btn-primary">
+                <Button>
+                  <h6>View Assignments</h6>
+                </Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </Row>
+    );
+  }
+}
 
 export default Course;
--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -14,7 +14,8 @@ import PrivateRoute from 'app/shared/auth/private-route';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import PageNotFound from 'app/shared/error/page-not-found';
 import { AUTHORITIES } from 'app/config/constants';
-import findUsername from "app/modules/account/find-username/findUsername";
+import findUsername from 'app/modules/account/find-username/findUsername';
+import Course from 'app/course/course';
 
 const Account = Loadable({
   loader: () => import(/* webpackChunkName: "account" */ 'app/modules/account'),
@@ -39,6 +40,7 @@ const Routes = () => {
         <ErrorBoundaryRoute path="/account/reset/finish/:key?" component={PasswordResetFinish} />
         <PrivateRoute path="/admin" component={Admin} hasAnyAuthorities={[AUTHORITIES.ADMIN]} />
         <PrivateRoute path="/account" component={Account} hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]} />
+        <PrivateRoute path="/home" component={Course} hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]} />
         <ErrorBoundaryRoute path="/" exact component={Home} />
         <PrivateRoute path="/" component={Entities} hasAnyAuthorities={[AUTHORITIES.USER]} />
         <ErrorBoundaryRoute component={PageNotFound} />
